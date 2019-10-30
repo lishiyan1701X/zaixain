b59b8cca8c24ccba089b4b74fa0020ed6635529c
lishiyan123 <2943308898@qq.com>
2019-10-30 10:46:52 +0800

    修改

---
+++ ocCommon/src/main/java/com/online/college/common/web/shiro/ShiroFreeMarkerConfigurer.java
@@ -0,0 +1,22 @@
+package com.online.college.common.web.shiro;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactory;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import com.online.college.common.web.shiro.freemarker.ShiroTags;
+
+import freemarker.template.TemplateException;
+
+/**
+ * shiro freemarker 整合
+ */
+public class ShiroFreeMarkerConfigurer extends FreeMarkerConfigurer {
+	@Override
+	public void afterPropertiesSet() throws IOException, TemplateException {
+		super.afterPropertiesSet();
+		this.getConfiguration().setSharedVariable("shiro", new ShiroTags());
+	}
+}
